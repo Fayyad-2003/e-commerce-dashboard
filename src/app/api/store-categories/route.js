5dc17b9f44662ebf24ba5008b289740a97e239a9
fayad <fayad>
2026-01-13 10:50:39 +0300

    ...

--- src/app/api/store-categories/route.js
+++ src/app/api/store-categories/route.js
@@ -21,3 +21,20 @@ export async function GET(req) {
         );
     }
 }
+
+export async function POST(req) {
+    try {
+        const formData = await req.formData();
+        const res = await serverFetch("/admin/store-categories/store", {
+            method: "POST",
+            body: formData,
+        });
+
+        return handleResponse(res);
+    } catch (err) {
+        return NextResponse.json(
+            { success: false, message: `استثناء: ${err?.message || err}` },
+            { status: 500 }
+        );
+    }
+}
